b3c65face714b3579ed0b81658fa49aa319c7706
Ben Anderson <banderson@constantcontact.com>
2015-05-20 01:16:39 -0400

    Rogue AppDispatcher references

--- store/templates/store.js
+++ store/templates/store.js
@@ -21,14 +21,14 @@ const <%= name %> = assign({}, BaseStore, {
   },
 
   // register store with dispatcher, allowing actions to flow through
-  dispatcherIndex: AppDispatcher.register(function(payload) {
+  dispatcherIndex: Dispatcher.register(function(payload) {
     let action = payload.action;
 
     switch(action.type) {
       case Constants.ActionTypes.ADD_TASK:
         let text = action.text.trim();
         // NOTE: if this action needs to wait on another store:
-        // AppDispatcher.waitFor([OtherStore.dispatchToken]);
+        // Dispatcher.waitFor([OtherStore.dispatchToken]);
         // For details, see: http://facebook.github.io/react/blog/2014/07/30/flux-actions-and-the-dispatcher.html#why-we-need-a-dispatcher
         if (text !== '') {
           addItem(text);
