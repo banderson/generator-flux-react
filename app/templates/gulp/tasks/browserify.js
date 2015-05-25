84ef4bc5eda729203b0c7f759b20d4442732702e
wmyers <ec23consulting@gmail.com>
2015-05-25 16:34:28 +0800

    refactored browserify gulp task to resolve issue where re-bundled js does not update in the dist folder

--- app/templates/gulp/tasks/browserify.js
+++ app/templates/gulp/tasks/browserify.js
@@ -7,17 +7,18 @@ var watchify = require('watchify');
 var connect = require('gulp-connect');
 var config = require('../config').browserify;
 
-watchify.args.debug = config.debug;
-var bundler = watchify(browserify(config.src, watchify.args));
+var b = browserify({ cache: {}, packageCache: {}, debug: config.debug });
+b.add(config.src);
 config.settings.transform.forEach(function(t) {
-  bundler.transform(t);
+  b.transform(t);
 });
+var w = watchify(b);
 
 gulp.task('browserify', bundle);
-bundler.on('update', bundle);
+w.on('update', bundle);
 
-function bundle() {
-  return bundler.bundle()
+function bundle(){
+  return w.bundle()
   // log errors if they happen
   .on('error', gutil.log.bind(gutil, 'Browserify Error'))
   .pipe(source(config.outputName))
